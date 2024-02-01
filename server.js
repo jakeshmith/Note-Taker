c14c0465c425b9ff5e618459e0cb126784928107
jakeshmith <153260507+jakeshmith@users.noreply.github.com>
2024-01-31 20:41:50 -0600

    finished project

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./02-Challenge/Routes/apiRoutes')(app);
-require('./02-Challenge/Routes/htmlRoutes')(app);
+require('./Routes/apiRoutes')(app);
+require('./Routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
     console.log(`Server available at localhost${PORT}`);
